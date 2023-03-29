b8ec8ad184c8ee6d51e940e9b8b3501bc0e055f5
gabrielmousquer <gabrielmousquer1227@gmail.com>
2023-03-29 14:17:37 -0300

    ✨ Configurando o context e colocando variavel de ambiente no .env

--- src/graphql/context/index.js
+++ src/graphql/context/index.js
@@ -1,4 +1,4 @@
-const { knex } = require('../../database/');
+const knex = require('../../database/index.js');
 
 const context = () => {
   return {
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-/* eslint-disable */
 const { ApolloServer } = require('apollo-server');
 const { resolvers, typeDefs } = require('./graphql/schemas.js');
+const { context } = require('./graphql/context/');
 require('dotenv').config();
 
 const server = new ApolloServer({ typeDefs, resolvers, context });
 
-server.listen({ port: process.env.SERVER_PORT }).then(({url}) => {
-  console.log('Server Started at' + url);
-});
+server
+  .listen({ port: process.env.SERVER_PORT })
+  .then(({ url }) => console.log('Server Started at ' + url));
