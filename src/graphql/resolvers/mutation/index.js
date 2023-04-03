d60382691779780dd78105782e87690cb5b2edcf
gabrielmousquer <gabrielmousquer1227@gmail.com>
2023-04-03 10:59:20 -0300

    ✨ Incluindo BCRYPT, para password hash.

--- src/graphql/resolvers/mutation/index.js
+++ src/graphql/resolvers/mutation/index.js
@@ -1,34 +1,37 @@
-const auth = async (_, { email }, { knex }) => {
-  return await knex('users')
+const auth = async (_, { email, password }, { knex, bcrypt }) => {
+  const userAuth = await knex('users')
     .first('id', 'email', 'password', 'username', 'role', 'avatar')
     .where({
       email,
     });
+  const hashedPassword = await bcrypt.compare(password, userAuth.password);
+  if (hashedPassword) {
+    return userAuth;
+  }
 };
 
-const register = async (_, args, { knex }) => {
+const register = async (_, { email, username, password }, { knex, bcrypt }) => {
   let verify = await knex('users').first('email').where({
-    email: args.email,
+    email,
   });
   if (!verify) {
-    await knex('users').insert(args);
+    const hashPassword = await bcrypt.hash(password, 10);
+    await knex('users').insert({ username, email, password: hashPassword });
     return false;
   }
   return true;
 };
 
-const edit = async (_, { id, avatar, username, email, password }, { knex }) => {
-  let [test] = await knex('users')
+const edit = async (
+  _,
+  { id, avatar, username, email, password },
+  { knex, bcrypt },
+) => {
+  const hash = await bcrypt.hash(password, 10);
+  const [test] = await knex('users')
     .where({ id })
-    .update({ avatar, username, email, password }, [
-      'avatar',
-      'username',
-      'email',
-      'password',
-      'role',
-      'id',
-      'created_at',
-    ]);
+    .returning(['avatar', 'username', 'email', 'password', 'id'])
+    .update({ avatar, username, email, password: hash });
   return test;
 };
 
