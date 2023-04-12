2c3870f354296f989f2be31bb659ec9b1de1e724
gabrielmousquer <gabrielmousquer1227@gmail.com>
2023-04-12 08:34:39 -0300

    ⚡️ Criando query para buscar linguages do usuario

--- src/graphql/resolvers/query/index.js
+++ src/graphql/resolvers/query/index.js
@@ -1,4 +1,11 @@
 const users = (_, __, { knex }) => {
   return knex('users').select('*');
 };
-module.exports = { users };
+
+const languagesUser = async (_, { id }, { knex }) => {
+  return await knex('users_languages')
+    .join('languages', 'languages.id', 'users_languages.language')
+    .where('user', id);
+};
+
+module.exports = { users, languagesUser };
