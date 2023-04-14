67f2c636ac3a0e50c1cbc7341f1ee6259f0be066
gabrielmousquer <gabrielmousquer1227@gmail.com>
2023-04-14 08:35:59 -0300

    🐛 Verificando se a linguagem esta vazia

--- src/graphql/resolvers/query/languagesUser.js
+++ src/graphql/resolvers/query/languagesUser.js
@@ -1,7 +1,12 @@
 const languagesUser = async (_, { id }, { knex }) => {
-  return await knex('users_languages')
+  const languages = await knex('users_languages')
     .join('languages', 'languages.id', 'users_languages.language')
     .where('user', id);
+  if (languages == '') {
+    return [{ id: 0 }];
+  }
+
+  return languages;
 };
 
 module.exports = { languagesUser };
