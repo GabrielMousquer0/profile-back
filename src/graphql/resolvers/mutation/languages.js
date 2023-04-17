a6594cb2cbdc4d001179c2f6a0bfb3bb9256d522
gabrielmousquer <gabrielmousquer1227@gmail.com>
2023-04-17 11:13:47 -0300

    🎨 Melhorando codigo

--- src/graphql/resolvers/mutation/languages.js
+++ src/graphql/resolvers/mutation/languages.js
@@ -1,21 +1,14 @@
 const editLanguages = async (_, { id, languages }, { knex }) => {
   await knex('users_languages').where({ user: id }).del();
-  const obj = {
-    js: languages.find((langId) => langId == 1),
-    py: languages.find((langId) => langId == 2),
-    ts: languages.find((langId) => langId == 3),
-  };
 
-  if (obj.js) {
-    await knex('users_languages').insert({ user: id, language: 1 });
-  }
-  if (obj.py) {
-    await knex('users_languages').insert({ user: id, language: 2 });
-  }
-  if (obj.ts) {
-    await knex('users_languages').insert({ user: id, language: 3 });
-  }
-  return true;
+  const insertData = languages.map((i) => {
+    return {
+      user: id,
+      language: i,
+    };
+  });
+
+  return !(await knex('users_languages').insert(insertData));
 };
 
 module.exports = { editLanguages };
