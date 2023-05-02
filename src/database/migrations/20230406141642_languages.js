502faf47e176f9d520da23ed265426299e998450
gabrielmousquer <gabrielmousquer1227@gmail.com>
2023-05-02 11:16:08 -0300

    ✨ Adicionando coluna para incluir icone da linguagem

--- src/database/migrations/20230406141642_languages.js
+++ src/database/migrations/20230406141642_languages.js
@@ -2,6 +2,7 @@ exports.up = function (knex) {
   return knex.schema.createTable('languages', function (table) {
     table.increments('id').primary();
     table.string('name');
+    table.string('icon');
   });
 };
 
