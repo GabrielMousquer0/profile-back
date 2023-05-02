f19619e8e7ffc50c4e6d527beaf0b7249f6673ef
gabrielmousquer <gabrielmousquer1227@gmail.com>
2023-05-02 11:41:08 -0300

    🔥 Codigo desnecessario

--- src/schema/resolvers/mutation/insertLanguages.js
+++ src/schema/resolvers/mutation/insertLanguages.js
@@ -1,11 +1,7 @@
 module.exports = {
   Mutation: {
     insertLanguages: async (_, { id, language }, { knex }) => {
-      if (
-        await knex('users_languages')
-          .first()
-          .where({ user: id, language: language })
-      ) {
+      if (await knex('users_languages').first().where({ user: id, language })) {
         return false;
       }
       await knex('users_languages').insert({ user: id, language });
