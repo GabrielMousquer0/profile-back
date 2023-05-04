0ed9fc58399107fb0f0f1174303c9d95fdb16439
gabrielmousquer <gabrielmousquer1227@gmail.com>
2023-05-04 14:45:19 -0300

    🐛 Ao criar um usuario ele insere no banco no id do user as configurações padrões "true"

--- src/schema/resolvers/mutation/register.js
+++ src/schema/resolvers/mutation/register.js
@@ -8,10 +8,20 @@ module.exports = {
           email,
         }))
       ) {
-        await knex('users').insert({
-          username,
-          email,
-          password: await bcrypt.hash(password, 10),
+        const [user] = await knex('users').insert(
+          {
+            username,
+            email,
+            password: await bcrypt.hash(password, 10),
+          },
+          ['id'],
+        );
+        await knex('users_options').insert({
+          user_id: user.id,
+          description: true,
+          role: true,
+          created_at: true,
+          languages: true,
         });
         return false;
       }
