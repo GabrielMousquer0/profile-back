3cb49bdad602915c3e0ee9f7857c962c82f15907
gabrielmousquer <gabrielmousquer1227@gmail.com>
2023-05-10 13:39:03 -0300

    ✨ Passando um input para o editPassword e fazendo verificação se a senha esta igual a nova

--- src/schema/resolvers/mutation/edit.js
+++ src/schema/resolvers/mutation/edit.js
@@ -8,10 +8,14 @@ module.exports = {
         .update(input, ['id', 'username', 'email', 'avatar']);
       return userUpdate;
     },
-    editPassword: async (_, { password, id }, { knex }) => {
+    editPassword: async (_, { input, id }, { knex }) => {
+      const userPassword = await knex('users').where({ id }).first('password');
+      if (await bcrypt.compare(input.current, userPassword.password)) {
+        return;
+      }
       const [passwordUpdate] = await knex('users')
         .where({ id })
-        .update({ password: await bcrypt.hash(password, 10) }, [
+        .update({ password: await bcrypt.hash(`123`, 10) }, [
           'id',
           'username',
           'email',
